ab1a57c9ebae93f3f6b170d996a38fce4854a1f2
santiago <santiagobenitez467@gmail.com>
2023-09-07 23:01:17 -0500

    modified:Dashboard/urls.py

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
+    path('', views.dashboard_view, name='index'),
     # Otros patrones de URL aquí
 ]
