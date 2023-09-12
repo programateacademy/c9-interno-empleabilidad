56b3bf83e2c2cb4877ea09e692a6e24956ccb3b7
KeviinSerna <kevinsernap@gmail.com>
2023-09-12 09:01:17 -0500

    change in Dashboard/views.py and Dashboard/urls.py

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('admin', views.dashboard)
+    path('admin', views.dashboard),
+    path('student', views.dashboardStudent),
     # Otros patrones de URL aquí
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def dashboard(request):
     return render (request, 'Dashboard/dashboard.html')
+
+def dashboardStudent(request):
+    return render (request, 'Dashboard/dashboardStudent.html')
