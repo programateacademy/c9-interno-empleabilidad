cd6a2f24337c63631115d5a03ca9bd1e913546d6
DaironRv <dairon.rodriguezvega@gmail.com>
2023-09-18 18:31:51 -0500

    javaScrip functions are added

--- Home/static/Js/script.js
+++ Home/static/Js/script.js
@@ -2,20 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     let contador = 0;
 
     function incrementarContador(event) {
-        // Prevenir la acción predeterminada del botón
         event.preventDefault();
 
         contador++;
         console.log(contador);
         if (contador === 3) {
             mostrarModal();
-            // Esto desactiva el boton a llegar a 3
             document.getElementById("Boton").disabled = true;
         }
     }
 
     function mostrarModal() {
-        // Aquí puedes poner el código para mostrar tu modal
         alert('El contador ha llegado a tres');
     }
 
