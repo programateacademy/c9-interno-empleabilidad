f94420734d68576d683e5241efe65f24bc332627
Daniel <dannyking07@outlook.es>
2023-09-23 16:27:12 -0500

    feat [Dashboard -> admin.py]

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,25 +1,10 @@
 from django.contrib import admin
-#from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 
 from .models.student import Estudiante
 from .models.cohorteDate import Cohorte
 from.models.company import Empresa
-
-# from .models.company import Empresa
-# admin.site.index_title = _('Bienvenido')
-# admin.site.site_header = _('Panel de administración de tu sitio')
-# admin.site.site_title = _('Panel de administración')
-
-# class MyAdminSite(admin.AdminSite):
-#     def has_permission(self, request):
-#         return request.user.is_active
-
-#     def has_module_permission(self, request):
-#         return False
-
-# admin_site = MyAdminSite(name='myadmin')
 
 class EstudianteAdmin(admin.ModelAdmin):
     list_display = ( 'nombre', 'dni', 'empleabilidad', 'empresa')
