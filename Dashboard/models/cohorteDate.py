2de4ca306e0ab1d5bdf7f851e087c27fb6959eb2
Daniel <dannyking07@outlook.es>
2023-09-23 16:28:49 -0500

    feat [Dashboard/models -> cohorteDate.py]

--- Dashboard/models/cohorteDate.py
+++ Dashboard/models/cohorteDate.py
@@ -2,13 +2,24 @@ from django.db import models
 from django.urls import reverse
 
 class Cohorte (models.Model):
+    OPCIONES = (
+        ('Activa', 'Activa'),
+        ('No Activa', ('No Activa'))
+    )
     numerodecohorte = models.IntegerField()
-    estadodelacorhorte = models.BooleanField(default=False)
+    estadodelacorhorte = models.CharField(max_length=15, choices=OPCIONES, default='Activa')
     numerodeparticipantes = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.numerodecohorte)
 
+    def obtener_palabra(self):
+        if self.seleccion == 'Activa':
+            return "Activa"
+        elif self.seleccion == 'No Activa':
+            return "No Activa"
+        else:
+            return "Ninguna opción seleccionada"
 
     def get_absolute_url(self):
         return reverse("cohorte_detail", args=[str(self.id)])
