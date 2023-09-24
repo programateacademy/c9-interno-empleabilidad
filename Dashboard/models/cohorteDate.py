cf8d4ae70669e66732659435f28ef775144574f3
Daniel <dannyking07@outlook.es>
2023-09-23 23:05:04 -0500

    feat [Dashboard/models -> cohorteDate]

--- Dashboard/models/cohorteDate.py
+++ Dashboard/models/cohorteDate.py
@@ -1,35 +1,42 @@
 from django.db import models
 from django.urls import reverse
 
-class Cohorte (models.Model):
-    OPCIONES = (
+class Cohorte(models.Model):
+    OPCIONES_ESTADO = (
         ('Activa', 'Activa'),
-        ('No Activa', ('No Activa'))
+        ('No Activa', 'No Activa')
     )
+
     numerodecohorte = models.IntegerField()
-    estadodelacorhorte = models.CharField(max_length=15, choices=OPCIONES, default='Activa')
-    numerodeparticipantes = models.IntegerField(default=0)
+    estadodelacorhorte = models.CharField(max_length=15, choices=OPCIONES_ESTADO, default='Activa')
+    numerodeestudiantes = models.PositiveIntegerField(editable=False, default=0)
 
     def __str__(self):
         return str(self.numerodecohorte)
 
+    def agregar_estudiante(self, estudiante):
+        if estudiante.cohorte != self:
+            estudiante.cohorte = self
+            estudiante.save()
+            self.actualizar_numerodeestudiantes()
+
+    def eliminar_estudiante(self, estudiante):
+        if estudiante.cohorte == self:
+            estudiante.cohorte = None
+            estudiante.save()
+            self.actualizar_numerodeestudiantes()
+
+    def actualizar_numerodeestudiantes(self):
+        self.numerodeestudiantes = self.estudiantes.count()
+        self.save()
+
     def obtener_palabra(self):
-        if self.seleccion == 'Activa':
+        if self.estadodelacorhorte == 'Activa':
             return "Activa"
-        elif self.seleccion == 'No Activa':
+        elif self.estadodelacorhorte == 'No Activa':
             return "No Activa"
         else:
             return "Ninguna opción seleccionada"
 
     def get_absolute_url(self):
         return reverse("cohorte_detail", args=[str(self.id)])
-
-    def add_participant (self):
-        self.numberParticipants += 1 
-        self.save()
-
-    def remove_participant (self):
-        if self.numberParticipants > 0:
-            self.numberParticipants -= 1
-            self.save()
-    
