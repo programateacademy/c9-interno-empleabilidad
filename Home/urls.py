b618e27849c4c597e78ec188992a33299efc78a7
Jasbbleidy <jasbbleidy.ca@gmail.com>
2023-09-24 23:09:28 -0500

    feat modified:   Home/urls.py

--- Home/urls.py
+++ Home/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('loginstudent/', views.loginstudent),
     path('home/', views.home),
-    
+    path('cambio_contraseña/', views.cambio_contraseña)
     # Otros patrones de URL aquí
 ]
