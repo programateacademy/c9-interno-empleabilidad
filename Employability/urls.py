20ae56747dce94e529ca32cb4cfe69a11242f475
Daniel <dannyking07@outlook.es>
2023-09-28 12:07:25 -0500

    feat [Employabilty -> urls.py]

--- Employability/urls.py
+++ Employability/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +26,5 @@ urlpatterns = [
 urlpatterns += [
     path('', include('Dashboard.urls')),
     path('', include('Home.urls')),
-
+    path('logout/', views.signout, name='logout'),
 ]
