ddece065a0041e4cbf5b44e9ab8cd2fd6841efa3
santiago <santiagobenitez467@gmail.com>
2023-09-27 19:09:56 -0500

    feat:modified:Home/urls.py

--- Home/urls.py
+++ Home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('loginstudent/', views.loginstudent, name='loginstudent'),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('', views.index, name='index')
     # Otros patrones de URL aquí
 ]
