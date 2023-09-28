7b64d51b904eab3cbe92ec10f8c9e1b77a995602
Daniel <dannyking07@outlook.es>
2023-09-27 19:20:19 -0500

    feat [Home -> views.py]

--- Home/views.py
+++ Home/views.py
@@ -1,38 +1,9 @@
-from django.shortcuts import render, redirect
-# from .forms import EstudianteLoginForm
-# from Dashboard.models.student import Estudiante
-from django.contrib.auth import authenticate, login
-
-# def loginstudent(request):
-
-#     return render(request, 'indexHome.html')
-
-# def loginstudent(request):
-#     authentication_form = EstudianteLoginForm
-#     return render(request, 'Home/indexHome.html')
-
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import EstudianteLoginForm
-
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-from .forms import EstudianteLoginForm
-
-# def loginstudent(request):
-#     if request.method == 'POST':
-#         form = EstudianteLoginForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username_or_dni')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('Home/home.html')
-#     else:
-#         form = EstudianteLoginForm()
-#     return render(request, 'indexHome.html', {'form': form})
 from django.contrib import messages
+from Dashboard.models.student import Estudiante
+from django.contrib.auth.decorators import login_required
 
 def loginstudent(request):
     if request.method == 'POST':
@@ -52,5 +23,8 @@ def loginstudent(request):
 
 
 
+@login_required
 def home(request):
-    return render (request, 'Home/home.html')
+    datos_usuario = Estudiante.objects.filter(username=request.user).first()
+    if datos_usuario:
+        return render(request, 'Home/home.html', {'datos_usuario': datos_usuario})
